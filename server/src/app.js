b21495aa3622e3170786a095f5daab56006ced3b
Mirza Sohaib Baig <mirzasohaib@example.com>
2026-07-24 21:09:37 +0500

    Week 2: Backend server setup completed

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "PulseLive API is Running 🚀",
+    });
+});
+
+export default app;
